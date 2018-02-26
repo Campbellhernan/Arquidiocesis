7b4a650d8b2a9ddb2a09fca35d246f8bc1c9e7cb
Campbellhernan <hcampbellhernan@gmail.com>
2018-02-25 23:53:48 -0400

    Cambios del Login
    Se cambio el tema y estilos de la imagen principal del login.

--- pages/login.php
+++ pages/login.php
@@ -8,98 +8,76 @@ if(isset($_SESSION['usuario'])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>SB Admin 2 - Bootstrap Admin Theme</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- MetisMenu CSS -->
-    <link href="../bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
-
+	<title>Iniciar sesión - Arquidiocesis</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+<!-- Bootstrap Core CSS -->
+<link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+<!--===============================================================================================-->
     <!-- Custom Fonts -->
     <link href="../bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-	
-	<style>
-		body {
-			background-image: url('../fondoScnueva.jpg');
-		}
-	
-		.letraRoja
-		{
-			color:red;
-		}
-	</style>
-
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../dist/css/util.css">
+	<link rel="stylesheet" type="text/css" href="../dist/css/main.css">
+<!--===============================================================================================-->
 </head>
-
 <body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-form-title" style="background-image: url(../bg-01.jpg);">
+					<span class="login100-form-title-1">
+						Arquidiocesis
+					</span>
+				</div>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div class="login-panel panel panel-default" style="padding-bottom:30px;">
-                    <div class="panel-heading" style="text-align:center; /*color:#FFC300;*/ padding-top:30px; padding-bottom:30px;">
-                        <h3 class="panel-title">Iniciar Sesión</h3>
+				<form class="login100-form validate-form" action="validarLogin.php" method="post">
+                    <label class="h3" style="padding-left:20%">Iniciar Sesión</label>
+                    <div>
+                        <?php
+                            if(isset($_REQUEST['error']))
+                                echo "<div class='alert alert-danger'>Usuario o contraseña invalidos</div>";
+                        ?>
                     </div>
-                    <div class="panel-body">
-                        <form role="form" action="validarLogin.php" method="post">
-                            <fieldset>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Usuario" name="usuario" type="text" autofocus>
-                                </div>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Contraseña" name="pass" type="password">
-                                </div>
-								<div>
-									<?php
-										if(isset($_REQUEST['error']))
-											echo "<p class='letraRoja'>Usuario o contraseña invalidos</p>";
-									?>
-								</div>
-                                <div class="checkbox">
-                                    <label>
-                                        <input name="remember" type="checkbox" value="Recuerdame">Recuerdame
-                                    </label>
-                                </div>
-                                <!-- Change this to a button or input when using this as a form -->
-                                <button type="submit" class="btn btn-lg btn-primary btn-block">Entrar</button>
-                            </fieldset>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+					<div class="wrap-input100 validate-input m-b-26" data-validate="El usuario es requerido">
+						<span class="label-input100">Usuario</span>
+						<input class="input100" type="text" name="usuario" placeholder="usuario" autofocus>
+						<span class="focus-input100"></span>
+					</div>
 
+					<div class="wrap-input100 validate-input m-b-18" data-validate = "La contraseña es requerida">
+						<span class="label-input100">Contraseña</span>
+						<input class="input100" type="password" name="pass" placeholder="contraseña">
+						<span class="focus-input100"></span>
+                    </div>
+                    <div class="flex-sb-m w-full p-b-30">
+						<div class="contact100-form-checkbox">
+							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+							<label class="label-checkbox100" for="ckb1">
+								Recuerdame
+							</label>
+						</div>
+					</div>
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn" type="submit">
+							Entrar
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+<!--===============================================================================================-->
     <!-- jQuery -->
     <script src="../bower_components/jquery/dist/jquery.min.js"></script>
-
+<!--===============================================================================================-->
     <!-- Bootstrap Core JavaScript -->
     <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../bower_components/metisMenu/dist/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="../dist/js/sb-admin-2.js"></script>
-
+<!--===============================================================================================-->
+    <script src="../dist/js/main.js"></script>
+<!--===============================================================================================-->    
 </body>
-
 </html>
